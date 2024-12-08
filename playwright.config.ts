b87f9fdd09c4d22f123d67254c447898930c01f2
robvieweg <rob.vieweg@gmail.com>
2024-12-08 13:43:06 +0100

    změny v configu pro reporting

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,30 +1,16 @@
-//toto je konfigurační soubor test runneru
-//musí se ukládaat do rootu projektu, test runner si ho pak sám najde
+import { defineConfig } from '@playwright/test';
 import { cpus } from "os";
-import { devices } from "@playwright/test";
 
-//testrunner se jmenuje "playwright/test" a je nativní
-//object obsahuje objekt, jehož properties slouží ke konfiguraci test runneru
-const config = {
-    //global configuration for all tests
+export default defineConfig({
+    testDir: "./tests", // Path to the test directory
+    workers: cpus().length, // Number of workers based on CPU cores
     use: {
         headless: false, 
-        //velikost okna browseru:
-        viewport: {width: 1920, height: 1080},
-        //ignorování případných chyb protokolu https:
+        viewport: { width: 1920, height: 1080 },
         ignoreHTTPSErrors: true,
-        //record video, screenshots and trace of tests
-        video:"on",
-        screenshot:"on",
-        trace:"on",
+        screenshot: "on", // Enable screenshots
+        trace: "on", // Enable trace for debugging
     },
-    //KONFIGURACE SAMOTNÉHO TEST RUNNERU:
-    //kde má hledat testy, které má spustit:
-    testDir: "/tests/loginTests",
-    //workers určují maximální počet paralelně exekuovaných souborů s testy
-    workers: cpus().length / 2, //pokud 1, pak testy poběží sekvenčně
-    
-    //KONFIGURACE TESTŮ = každý test poběží dle vlastní konfigurace na jednom ze zařízení níže:
     projects: [
         {
             name: "Chromium Desktop",
@@ -33,28 +19,9 @@ const config = {
                 headless: false,
             },
         },
-        {
-            name: "Webkit Desktop",
-            use: {
-                browserName: "webkit"
-            },
-        },
-        {
-            name: "Firefox Desktop 800x600",
-            use: {
-                browserName: "firefox",
-                viewport: {width: 800, height: 600},
-            },
-        },
-        {
-            name: "Webkit iPhone 12",
-            use: {
-                browserName: "webkit",
-                ...devices["iPhone 12"] //objekt devices se naimportoval ... 3 tečky = destructuring = vložíme všechny properties do objektu, před který jsme napsali 3 tečky
-            }
-        }
     ],
-};
-
-//musíme ho exportovat, aby ho test runner mohl při každém spuštění importovat
-export default config;
+    reporter: [
+        ['list'], // Console output
+        ['json', { outputFile: 'test-results/test-results.json' }]
+    ],
+});
